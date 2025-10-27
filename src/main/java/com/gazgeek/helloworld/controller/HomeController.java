33cae2a0c14b9d7aca4caa9978714e76290f0427
aishpaym <48062487+aishpaym@users.noreply.github.com>
2025-10-27 14:08:26 +0530

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from Aishwarya Sasankan!";
+        return "Hello from Aishwarya Sasankan Test!";
     }
 
 }
